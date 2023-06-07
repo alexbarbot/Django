b9780511f0c2680368d23609b0e542550082c6cb
alexbarbot <alex.barbot@etu.univ-poitiers.fr>
2023-06-07 23:02:11 +0200

    commentaire du code et ajout de appart

--- computerApp/forms.py
+++ computerApp/forms.py
@@ -3,66 +3,87 @@ from django.core.exceptions import ValidationError
 from .models import Machine, Personnel
 from datetime import datetime, timezone
 import computerApp.choices as choices
+import re
 
+# Classe de widget pour le champ de date
 class DateInput(forms.DateInput):
     input_type = 'date'
-    
-class AddMachineForm (forms.Form) : 
-    
+
+# Formulaire pour ajouter une machine
+class AddMachineForm(forms.Form):
     maintenanceDate = forms.DateField(widget=DateInput, label='Date de maintenance')
     nom = forms.CharField(required=True, label='Nom de la machine')
     mach = forms.ChoiceField(choices=choices.TYPE, label='Type')
-    etat = forms.BooleanField(required=False, label="état de la machine")
+    etat = forms.BooleanField(required=False, label="État de la machine")
     ip = forms.GenericIPAddressField(label='Adresse IP')
-    #appart = forms.ModelChoiceField(queryset=Personnel.objects.all(),required=True, label="appartient à ")
-   
+    appart = forms.ModelChoiceField(queryset=Personnel.objects.all(), required=True, label="Appartient à")
+
     def clean_nom(self):
+        # Validation du champ "nom" pour s'assurer qu'il ne dépasse pas 20 caractères
         data = self.cleaned_data["nom"]
         if len(data) > 20:
-            raise ValidationError(("erreur de format pour le champ nom"))
+            raise ValidationError(("Erreur de format pour le champ nom"))
         return data
-    
+
     def clean_maintenanceDate(self):
+        # Validation du champ "maintenanceDate" pour s'assurer qu'il est supérieur ou égal à la date actuelle
         data = self.cleaned_data["maintenanceDate"]
         if data < datetime.today().date():
             raise ValidationError(_("Erreur de format pour le champ maintenanceDate"))
         return data
-    
-    def cleaned_ip(self):
+
+    def clean_ip(self):
+        # Validation du champ "ip" pour s'assurer qu'il correspond au format d'une adresse IP valide
         data = self.cleaned_data["ip"]
         regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
         if not (re.search(regex, data)):
             raise ValidationError(_("Erreur de format pour le champ maintenanceDate"))
         return data
-    
-    def cleaned_mach(self):
+
+    def clean_mach(self):
+        # Validation du champ "mach" pour s'assurer qu'il correspond à une valeur valide parmi les choix disponibles
         data = self.cleaned_data["mach"]
         if data not in choices.TYPE:
             raise ValidationError(_("Erreur de format pour le champ maintenanceDate"))
         return data
+    
+    def clean_appart(self):
+        # Validation du champ "appart" pour s'assurer qu'une valeur valide a été sélectionnée
+        data = self.cleaned_data["appart"]
+        if data is None:
+            raise ValidationError(_("Erreur de format pour le champ appart"))
+        return data
 
-class AddPersonnelForm (forms.Form) : 
 
+# Formulaire pour ajouter du personnel
+class AddPersonnelForm(forms.Form):
     nom = forms.CharField(required=True, label='Nom du personnel')
-    mail = forms.EmailField(max_length=50, label = 'nom du mail')
+    mail = forms.EmailField(max_length=50, label='Nom du mail')
+
     def clean_nom(self):
+        # Validation du champ "nom" pour s'assurer qu'il ne dépasse pas 10 caractères
         data = self.cleaned_data["nom"]
         if len(data) > 10:
-            raise ValidationError(("erreur de format pour le champ nom"))
-        return data
-    
-    def clean_mail(self):
-        data = self.cleaned_data["mail"]
-        if len(data) > 20:
-            raise ValidationError(("erreur de format pour le champ nom"))
+            raise ValidationError(("Erreur de format pour le champ nom"))
         return data
 
+    def clean_mail(self):
+        # Validation du champ "mail" pour s'assurer qu'il ne dépasse pas 20 caractères
+        data = self.cleaned_data["mail"]
+        if len(data) > 20:
+            raise ValidationError(("Erreur de format pour le champ nom"))
+        return data
+
+
+# Formulaire pour supprimer une ou plusieurs machines
 class DeleteMachineForm(forms.Form):
     machines = forms.ModelMultipleChoiceField(
         queryset=Machine.objects.all(),
         widget=forms.CheckboxSelectMultiple
     )
 
+
+# Formulaire pour supprimer du personnel
 class DeletePersonnelForm(forms.Form):
     personnels = forms.ModelMultipleChoiceField(
         queryset=Personnel.objects.all(),
--- computerApp/views.py
+++ computerApp/views.py
@@ -2,97 +2,127 @@ from django.shortcuts import render, get_object_or_404, redirect
 from computerApp.models import Machine, Personnel
 from .forms import AddMachineForm, AddPersonnelForm, DeleteMachineForm, DeletePersonnelForm
 
+# Vue pour la page d'accueil
+def index(request):
+    # Récupérer tous les objets Personnel et Machine depuis la base de données
+    personnels = Personnel.objects.all()
+    machines = Machine.objects.all()
+    # Passer les objets récupérés au template 'index.html'
+    context = {
+        'personnel': personnels,
+        'machines': machines,
+    }
+    return render(request, 'index.html', context)
 
-# Create your views here.
+# Vue pour afficher la liste des machines
+def machine_list_view(request):
+    # Récupérer tous les objets Machine depuis la base de données
+    machines = Machine.objects.all()
+    # Passer les machines récupérées au template 'computerApp/machines_list.html'
+    context = {'machines': machines}
+    return render(request, 'computerApp/machines_list.html', context)
 
-def index(request) :
-	personnels = Personnel.objects.all()
-	machines = Machine.objects.all()
-	context = {
-		'personnel' : personnels,
-		'machines' : machines, 
-	}
-	return render(request, 'index.html', context)
-
-def machine_list_view(request) :
-	machines = Machine.objects.all()
-	context = {'machines': machines}
-	return render(request, 'computerApp/machines_list.html', context)
-
+# Vue pour afficher les détails d'une machine spécifique
 def machine_detail_view(request, pk):
-	machine = get_object_or_404(Machine, id=pk)
-	context={'machine': machine}
-	return render(request , 'computerApp/machines_detail.html',context)
+    # Récupérer la machine correspondante à l'ID fourni (ou retourner une erreur 404 si elle n'existe pas)
+    machine = get_object_or_404(Machine, id=pk)
+    # Passer la machine récupérée au template 'computerApp/machines_detail.html'
+    context = {'machine': machine}
+    return render(request, 'computerApp/machines_detail.html', context)
 
-def personnel_list_view(request) :
-	personnel = Personnel.objects.all()
-	context={'personnels': personnel}
-	return render(request, 'computerApp/personnel_list.html',context)
+# Vue pour afficher la liste du personnel
+def personnel_list_view(request):
+    # Récupérer tous les objets Personnel depuis la base de données
+    personnel = Personnel.objects.all()
+    # Passer le personnel récupéré au template 'computerApp/personnel_list.html'
+    context = {'personnels': personnel}
+    return render(request, 'computerApp/personnel_list.html', context)
 
+# Vue pour afficher les détails d'une personne spécifique
 def personnel_detail_view(request, pk):
-	personnel = get_object_or_404(Personnel, id=pk)
-	context={'personnel': personnel}
-	return render(request, 'computerApp/personnel_detail.html', context)
+    # Récupérer la personne correspondante à l'ID fourni (ou retourner une erreur 404 si elle n'existe pas)
+    personnel = get_object_or_404(Personnel, id=pk)
+    # Passer la personne récupérée au template 'computerApp/personnel_detail.html'
+    context = {'personnel': personnel}
+    return render(request, 'computerApp/personnel_detail.html', context)
 
+# Vue pour ajouter une nouvelle machine
 def machine_add_view(request):
     if request.method == 'POST':
         form = AddMachineForm(request.POST)
         if form.is_valid():
+            # Récupérer les données valides du formulaire
             nom = form.cleaned_data['nom']
             mach = form.cleaned_data['mach']
             etat = form.cleaned_data['etat']
-            new_machine = Machine(nom=nom,mach=mach,etat=etat)
+            # Créer une nouvelle instance de Machine avec les données récupérées
+            new_machine = Machine(nom=nom, mach=mach, etat=etat)
             new_machine.save()
+            # Rediriger vers la page affichant la liste des machines
             return redirect('machines')
     else:
+        # Si la requête n'est pas une requête POST, créer un formulaire vide
         form = AddMachineForm()
     
+    # Passer le formulaire au template 'computerApp/machines_list.html'
     context = {'form': form}
     return render(request, 'computerApp/machines_list.html', context)
 
+# Vue pour ajouter une nouvelle personne
 def personnel_add_view(request):
     if request.method == 'POST':
         form = AddPersonnelForm(request.POST)
         if form.is_valid():
+            # Récupérer les données valides du formulaire
             nom = form.cleaned_data['nom']
+            # Créer une nouvelle instance de Personnel avec les données récupérées
             new_personnel = Personnel(nom=nom)
             new_personnel.save()
+            # Rediriger vers la page affichant la liste du personnel
             return redirect('personnels')
     else:
+        # Si la requête n'est pas une requête POST, créer un formulaire vide
         form = AddPersonnelForm()
     
+    # Passer le formulaire au template 'computerApp/personnel_list.html'
     context = {'form': form}
     return render(request, 'computerApp/personnel_list.html', context)
 
+# Vue pour supprimer des personnes
 def personnel_delete_view(request):
     if request.method == 'POST':
+        # Récupérer les identifiants des personnes sélectionnées dans le formulaire
         personnels_ids = request.POST.getlist('personnels')
+        # Filtrer les personnes correspondantes dans la base de données et les supprimer
         personnels = Personnel.objects.filter(id__in=personnels_ids)
         personnels.delete()
+        # Rediriger vers la page affichant la liste du personnel
         return redirect('personnels')
 
+    # Récupérer tous les objets Personnel depuis la base de données
     personnels = Personnel.objects.all()
+    # Passer le personnel récupéré au template 'computerApp/personnel_list.html'
     context = {'personnels': personnels}
     return render(request, 'computerApp/personnel_list.html', context)
 
+# Vue pour supprimer des machines
 def machine_delete_view(request):
     if request.method == 'POST':
+        # Récupérer les identifiants des machines sélectionnées dans le formulaire
         machines_ids = request.POST.getlist('machines')
+        # Filtrer les machines correspondantes dans la base de données et les supprimer
         machines = Machine.objects.filter(id__in=machines_ids)
         machines.delete()
+        # Rediriger vers la page affichant la liste des machines
         return redirect('machines')
 
+    # Récupérer tous les objets Machine depuis la base de données
     machines = Machine.objects.all()
+    # Passer les machines récupérées au template 'computerApp/machines_list.html'
     context = {'machines': machines}
     return render(request, 'computerApp/machines_list.html', context)
 
+# Vue pour la gestion
 def gestion_view(request):
     context = {}
     return render(request, 'computerApp/gestion.html', context)
-
-
-
-
-
-
-
